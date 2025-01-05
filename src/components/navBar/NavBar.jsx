f176973a55deabda77fbbe0be98540ff064970a8
Sanjay Sanjay <sanjay.05@icloud.com>
2025-01-05 11:59:03 +0530

    added hover effect

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -7,10 +7,12 @@ const NavBar = () => {
   return (
     <nav className='p-2 border-b dark:border-b-gray-700 bg-white dark:bg-black dark:text-white'>
       <ul className='flex justify-between'>
-        <li className='flex gap-1 items-center'><SlArrowLeft /></li>
+        <li className='flex gap-1 items-center hover:bg-gray-200 dark:hover:bg-gray-700 w-10 h-10 justify-center rounded-full'><SlArrowLeft /></li>
         <li className='text-xl font-bold'>Notes App</li>
         <li className='flex gap-2 items-center'>
-          <PiNotePencilThin className='w-6 h-6' />
+          <div className='p-2 hover:dark:bg-gray-700 hover:bg-gray-200 rounded-full'>
+            <PiNotePencilThin className='w-6 h-6' />
+          </div>
           <img width={30} height={30} src={profile} alt="" />
         </li>
       </ul>
