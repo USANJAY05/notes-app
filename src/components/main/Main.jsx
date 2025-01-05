c648ff2a6ad3e0e34dbe44f0b4140f726c706bb5
Sanjay Sanjay <sanjay.05@icloud.com>
2025-01-05 22:27:43 +0530

    added reactQuill

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const Main = () => {
-  return (
-    <div className='w-full h-full dark:bg-black dark:text-white p-2'>Content</div>
-  )
-}
+  const [note, setNote] = useState('');
 
-export default Main
+  // Handle changes in the editor
+  const handleEditorChange = (value) => {
+    setNote(value);
+    console.log(value)
+  };
+
+  // Custom toolbar options with more styling options
+  const modules = {
+    toolbar: [
+      [{ 'header': '1' }, { 'header': '2' }, { 'font': ['Arial', 'Courier', 'Georgia', 'Impact', 'Times New Roman', 'Verdana'] }],
+      [{ 'size': ['small', 'medium', 'large', 'huge'] }], // Font sizes
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }], // Ordered list, bullet list, and checkboxes
+      ['bold', 'italic', 'underline', 'strike'], // Added strikethrough
+      [{ 'align': [] }],
+      ['link', 'image'],
+      [{ 'color': [] }, { 'background': [] }], // Text color and background color
+      ['blockquote', 'code-block'], // Blockquote and code block options
+      ['clean'], // To clear the editor
+      ['unordered-list', 'ordered-list', 'description-list'] // New lists added
+    ],
+  };
+
+  return (
+    <div className='w-full h-full dark:bg-black dark:text-white'>
+      <div className="h-full overflow-auto">
+        {/* Fixed toolbar section */}
+        <div className="quill-toolbar h-[calc(100%-50px)]">
+          <ReactQuill 
+            value={note} 
+            onChange={handleEditorChange} 
+            theme="snow" 
+            modules={modules}  
+            placeholder="Write your note here..." 
+            className='h-full border-none'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
