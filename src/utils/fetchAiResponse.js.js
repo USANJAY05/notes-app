9616b644c5e7b4479298d8b55f88622e2153ebad
Sanjay Sanjay <sanjay.05@icloud.com>
2025-01-06 19:31:04 +0530

    removed api

--- src/utils/fetchAiResponse.js.js
+++ src/utils/fetchAiResponse.js.js
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const fetchAIResponse = async ( prompt) => {
   try {
-    const genAI = new GoogleGenerativeAI("AIzaSyAtnYr_9_Q23Je8VUZB0JIgjrP14zINf6o"); // Initialize with API key
+    const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY); // Initialize with API key
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" }); // Select the model
     const result = await model.generateContent(prompt); // Generate content
     return result.response.text(); // Return the generated response
