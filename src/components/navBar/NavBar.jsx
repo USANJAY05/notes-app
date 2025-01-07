30794efd938ae114740634f3512a46853155e84b
Sanjay Sanjay <sanjay.05@icloud.com>
2025-01-07 22:25:58 +0530

    updated

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import { SlArrowLeft } from "react-icons/sl";
 import profile from '../../assets/contact.png'
 import { PiNotePencilThin } from "react-icons/pi";
+import { useNavigate } from 'react-router';
 
 const NavBar = () => {
+  const navigate = useNavigate()
   return (
     <nav className='p-2 border-b dark:border-b-gray-700 bg-white dark:bg-black dark:text-white'>
       <ul className='flex justify-between'>
-        <li className='flex gap-1 items-center hover:bg-gray-200 dark:hover:bg-gray-700 w-10 h-10 justify-center rounded-full'><SlArrowLeft /></li>
+        <li className='flex gap-1 items-center hover:bg-gray-200 dark:hover:bg-gray-700 w-10 h-10 justify-center rounded-full'><SlArrowLeft onClick={() => navigate('/')} /></li>
         <li className='text-xl font-bold'>Notes App</li>
         <li className='flex gap-2 items-center'>
           <div className='p-2 hover:dark:bg-gray-700 hover:bg-gray-200 rounded-full'>
-            <PiNotePencilThin className='w-6 h-6' />
+            <PiNotePencilThin onClick={() => navigate('/')} className='w-6 h-6' />
           </div>
           <img width={30} height={30} src={profile} alt="" />
         </li>
--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router'
+
 
 const SideBar = () => {
+    const notes = useSelector((state) => state.items.notes);
+    console.log(notes)
+  const navigate = useNavigate()
+
   return (
     <div className='w-64 h-full border-r dark:border-r-gray-700 dark:bg-black border-b-gray-50 p-2 box-border overflow-auto'>
-        <div className="p-2 mb-1 w-full dark:text-white bg-gray-200 hover:bg-gray-300 dark:bg-gray-500 dark:hover:bg-gray-600 hover:cursor-pointer rounded-md">hello</div>
+      {notes.map(item =>(
+        <div 
+          key={item.id} 
+          onClick={()=>navigate(`/notes/${item.id}`)}
+          className="p-2 mb-1 w-full dark:text-white bg-gray-200 hover:bg-gray-300 dark:bg-gray-500 dark:hover:bg-gray-600 hover:cursor-pointer rounded-md"
+        >{item.content.slice(0,20)+"..."}</div>
+      ))}
     </div>
   )
 }
