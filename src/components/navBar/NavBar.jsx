05ffae8b1c2a97df80da88667f22cb2f41f3d313
Sanjay Sanjay <sanjay.05@icloud.com>
2025-01-08 16:56:05 +0530

    handle setNotes

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -6,29 +6,43 @@ import { useNavigate } from 'react-router';
 import { useDispatch } from 'react-redux';
 import { toggleFalse, toggleSideBar, toggleTrue } from '../../redux/slice/sideToggle-slice';
 import useMobileSize from '../../hooks/useMobileSize';
+import { setNote } from '../../redux/slice/note-slice';
 
 const NavBar = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const mobileSize = useMobileSize()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const mobileSize = useMobileSize();
+
   return (
     <nav className='p-2 border-b dark:border-b-gray-700 bg-white dark:bg-black dark:text-white'>
       <ul className='flex justify-between'>
-        <li onClick={() => (dispatch(toggleTrue()),navigate('/'))} className='flex items-center  justify-center rounded-full hover:text-blue-500 cursor-pointer'>
+        <li onClick={() => {
+          dispatch(toggleTrue());
+          navigate('/');
+        }} className='flex items-center justify-center rounded-full hover:text-blue-500 cursor-pointer'>
           <div className='w-6 flex justify-center items-center'>
             <SlArrowLeft />
           </div>
           <h1>Notes</h1>
-          </li>
+        </li>
         <li className='flex gap-2 items-center'>
           <div className='p-2 hover:dark:bg-gray-700 hover:bg-gray-200 rounded-full'>
-            <PiNotePencilThin onClick={() => (mobileSize&&dispatch(toggleFalse()),navigate('/'))} className='w-6 h-6 hover:cursor-pointer' />
+            <PiNotePencilThin 
+              onClick={() => {
+                if (mobileSize) dispatch(toggleFalse());
+                navigate('/');
+                setTimeout(() => {
+                  dispatch(setNote('')); // Dispatching the setNote action properly
+                }, 10);
+              }} 
+              className='w-6 h-6 hover:cursor-pointer' 
+            />
           </div>
-          <img className='cursor-pointer' width={30} height={30} src={profile} alt="" />
+          <img className='cursor-pointer' width={30} height={30} src={profile} alt="Profile" />
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
